4e960e33132638b1b9e6acc646df3fa9ed16ebe4
EvaMsanchez <eva_crm20@hotmail.com>
2024-10-21 13:44:10 +0200

    Advice After

--- src/main/java/com/eva/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/eva/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -3,6 +3,7 @@ package com.eva.curso.springboot.app.aop.springboot_aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -16,9 +17,11 @@ public class GreetingAspect
     // Con esto podemos registrar los eventos
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    // Registre un loggin antes de llamar al método
+    // Registre un loggin ANTES de llamar al método
     // JoinPoint: punto de unión. Une el aspecto con el método
-    @Before("execution(* com.eva.curso.springboot.app.aop.springboot_aop.services.GreetingService.sayHello(..))") // punto de corte
+    // @Before("execution(* com.eva.curso.springboot.app.aop.springboot_aop.services.*.*(..))") // se aplica a cualquier clase dentro de la carpeta service y a cualquier método de esa clase
+    // @Before("execution(* com.eva.curso.springboot.app.aop.springboot_aop.services.GreetingService.sayHello(..))") // punto de corte
+    @Before("execution(* com.eva.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))") // punto de corte
     public void loggerBefore(JoinPoint joinPoint)
     {
         // Obtener el nombre del método
@@ -26,5 +29,17 @@ public class GreetingAspect
         String args = Arrays.toString(joinPoint.getArgs());
 
         logger.info("Antes: " + method + " con los argumentos " + args);
+    }
+
+
+    // Realiza algo DESPUÉS de llamar al método, SIEMPRE, haya o no error
+    @After("execution(* com.eva.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))") // punto de corte
+    public void loggerAfter(JoinPoint joinPoint)
+    {
+        // Obtener el nombre del método
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("Después: " + method + " con los argumentos " + args);
     }
 }
